89cb772a3759f603d46fc44f118ef985c1b9d2a0
Eskizo <arturgb0012@gmail.com>
2025-07-17 13:08:10 -0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/eskizobr/Fluent/refs/heads/main/main.lua"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/eskizobr/Fluent/refs/heads/main/fluent.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/eskizobr/Fluent/refs/heads/main/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/eskizobr/Fluent/refs/heads/main/Addons/InterfaceManager.lua"))()
 
